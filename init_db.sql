d666132b54e6ede1ea5abc7816cd143f888e6acd
root <natesdeklen@gmail.com>
2026-03-22 21:21:56 +0000

    Added initial db schema for users and cards

---
+++ init_db.sql
@@ -0,0 +1,29 @@
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password_hash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE inventory (
+	id INT AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	oracle_id VARCHAR(36) NOT NULL,
+	quantity INT DEFAULT 1 NOT NULL,
+	card_name VARCHAR(255) NOT NULL,
+	type_line VARCHAR(255) NOT NULL,
+	mana_cost INT DEFAULT 0 NOT NULL,
+	rarity CHAR DEFAULT 'C' NOT NULL,
+	text_box TEXT,
+	power INT,
+	toughness INT,
+	w BOOLEAN,
+	u BOOLEAN,
+	b BOOLEAN,
+	r BOOLEAN,
+	g BOOLEAN,
+
+	PRIMARY KEY (id),
+	CONSTRAINT owned_by FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	CONSTRAINT user_card_id UNIQUE (user_id, oracle_id),
+	CONSTRAINT positive_cards CHECK (quantity > 0)
+);
