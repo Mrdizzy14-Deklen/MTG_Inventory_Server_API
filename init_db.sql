82b9520c6aa0ae4c5bbb10afee13289f1767e4c4
Deklen Nates <dmn230001@utdallas.edu>
2026-03-24 03:20:42 -0500

    Normalized data to save storage space

--- init_db.sql
+++ init_db.sql
@@ -9,21 +9,10 @@ CREATE TABLE inventory (
 	user_id INT NOT NULL,
 	oracle_id VARCHAR(36) NOT NULL,
 	quantity INT DEFAULT 1 NOT NULL,
-	card_name VARCHAR(255) NOT NULL,
-	type_line VARCHAR(255) NOT NULL,
-	mana_cost INT DEFAULT 0 NOT NULL,
-	rarity CHAR DEFAULT 'C' NOT NULL,
-	text_box TEXT,
-	power INT,
-	toughness INT,
-	w BOOLEAN,
-	u BOOLEAN,
-	b BOOLEAN,
-	r BOOLEAN,
-	g BOOLEAN,
-
+	
 	PRIMARY KEY (id),
 	CONSTRAINT owned_by FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	CONSTRAINT card_info FOREIGN KEY (oracle_id) REFERENCES ref_cards(oracle_id),
 	CONSTRAINT user_card_id UNIQUE (user_id, oracle_id),
 	CONSTRAINT positive_cards CHECK (quantity > 0)
 );
@@ -33,15 +22,15 @@ CREATE TABLE ref_cards (
     card_name VARCHAR(255) NOT NULL,
     type_line VARCHAR(255) NOT NULL,
     mana_cost INT DEFAULT 0 NOT NULL,
-    rarity CHAR DEFAULT 'C' NOT NULL,
+    rarity VARCHAR(20) NOT NULL,
 	text_box TEXT,
-	power INT,
-	toughness INT,
-	w BOOLEAN,
-	u BOOLEAN,
-	b BOOLEAN,
-	r BOOLEAN,
-	g BOOLEAN,
+	power VARCHAR(10),
+	toughness VARCHAR(10),
+	w BOOLEAN DEFAULT 0,
+	u BOOLEAN DEFAULT 0,
+	b BOOLEAN DEFAULT 0,
+	r BOOLEAN DEFAULT 0,
+	g BOOLEAN DEFAULT 0,
 
 	PRIMARY KEY (oracle_id)
 );
