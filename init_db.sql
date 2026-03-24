b102bc93cdfe6cc61435f7764e0b535906b56138
Deklen Nates <dmn230001@utdallas.edu>
2026-03-24 04:04:27 -0500

    Fixed order of table creation

--- init_db.sql
+++ init_db.sql
@@ -4,19 +4,6 @@ CREATE TABLE users (
 	password_hash VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE inventory (
-	id INT AUTO_INCREMENT,
-	user_id INT NOT NULL,
-	oracle_id VARCHAR(36) NOT NULL,
-	quantity INT DEFAULT 1 NOT NULL,
-	
-	PRIMARY KEY (id),
-	CONSTRAINT owned_by FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-	CONSTRAINT card_info FOREIGN KEY (oracle_id) REFERENCES ref_cards(oracle_id),
-	CONSTRAINT user_card_id UNIQUE (user_id, oracle_id),
-	CONSTRAINT positive_cards CHECK (quantity > 0)
-);
-
 CREATE TABLE ref_cards (
 	oracle_id VARCHAR(36) NOT NULL,
     card_name VARCHAR(255) NOT NULL,
@@ -33,6 +20,19 @@ CREATE TABLE ref_cards (
 	g BOOLEAN DEFAULT 0,
 
 	PRIMARY KEY (oracle_id)
+);
+
+CREATE TABLE inventory (
+	id INT AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	oracle_id VARCHAR(36) NOT NULL,
+	quantity INT DEFAULT 1 NOT NULL,
+	
+	PRIMARY KEY (id),
+	CONSTRAINT owned_by FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+	CONSTRAINT card_info FOREIGN KEY (oracle_id) REFERENCES ref_cards(oracle_id),
+	CONSTRAINT user_card_id UNIQUE (user_id, oracle_id),
+	CONSTRAINT positive_cards CHECK (quantity > 0)
 );
 
 CREATE TABLE meta_data (
