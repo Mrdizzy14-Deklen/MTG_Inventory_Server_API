cd39cebcc600a4701206716dd2e3175829e4c539
Deklen Nates <dmn230001@utdallas.edu>
2026-03-24 22:11:04 -0500

    Time zone updated

--- api.py
+++ api.py
@@ -59,7 +59,7 @@ def verify_password(plain_password: str, hashed_password: str):
 # Creates a JWT access token
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(hours=2)
+    expire = datetime.datetime.now(datetime.UTC) + timedelta(hours=2)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
